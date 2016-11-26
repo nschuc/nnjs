252b63fdeb2e876894a4c96b040c3b84884331de
Nathan Schucher <nathan.schucher@gmail.com>
2016-11-26 03:48:55 -0500

    Add flow type checking

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
     libraryTarget: "var",
     library: "nn"
   },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+  },
   module: {
     loaders: [{
       exclude: /node_modules/,
@@ -15,6 +19,7 @@ module.exports = {
         presets: ['es2015']
       }
     },
+    { test: /\.ts$/, loader: 'ts-loader' },
     { test: /\.json$/, loader: 'json' },
     ]
   }
