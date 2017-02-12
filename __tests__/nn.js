3cc89f0c729a27bbf78270611449b5bd836ed333
Nathan Schucher <nathan.schucher@gmail.com>
2017-02-11 23:16:59 -0500

    Clean up nn test

--- __tests__/nn.js
+++ __tests__/nn.js
@@ -1,19 +1,19 @@
-import Tensor from '../src/tensor.js';
-import { Variable } from '../src/autodiff';
-import { closeEnough, matTestData, mmData } from './data.js';
-import nn from '../';
-import nj from 'numjs';
+import Tensor from "../src/tensor.js";
+import { Variable } from "../src/autodiff";
+import { closeEnough, matTestData, mmData } from "./data.js";
+import nn from "../";
+import nj from "numjs";
 
-describe('variable op mixins', () => {
-  it('should have an add operation', () => {
-    let t1 = new Variable(new Tensor({ shape: [5, 3] }));
-    let t2 = new Variable(new Tensor({ shape: [5, 3] }));
+describe("variable op mixins", () => {
+  it("should have an add operation", () => {
+    let t1 = new Variable(new Tensor({ shape: [ 5, 3 ] }));
+    let t2 = new Variable(new Tensor({ shape: [ 5, 3 ] }));
     let t3 = t1.add(t2);
   });
-})
+});
 
-describe('variable addition', () => {
-  it('should have an add operation', () => {
+describe("variable addition", () => {
+  it("should have an add operation", () => {
     let t1 = new Tensor({ data: matTestData.a });
     let t2 = new Tensor({ data: matTestData.b });
 
@@ -24,29 +24,29 @@ describe('variable addition', () => {
     expect(closeEnough(res, matTestData.a_plus_b)).toBe(true);
   });
 
-  it('should have a matmul operation', () => {
+  it("should have a matmul operation", () => {
     let W = new Variable(new Tensor({ data: mmData.W }));
     let x = new Variable(new Tensor({ data: mmData.x }));
     let y = W.matmul(x);
     expect(closeEnough(y.data.numjs(), mmData.y)).toBe(true);
   });
-})
+});
 
-describe('variable gradient', () => {
-  it('should have grad set when backward is called', () => {
+describe("variable gradient", () => {
+  it("should have grad set when backward is called", () => {
     let x = new Variable(nn.ones(3));
     let y = x.mul(2);
-    while( y.data.norm() < 1000 ) {
+    while (y.data.norm() < 1000) {
       y = y.mul(2);
     }
 
-    const gradients = nn.fromArray([0.1, 1, 0.0001])
+    const gradients = nn.fromArray([ 0.1, 1, 0.0001 ]);
 
     y.backward(gradients);
 
     const grad = x.grad.numjs();
-    console.log(grad);
-    const expectedGrad = nj.array([102.4, 1024.0, 0.1024]);
+    const expectedGrad = nj.array([ 102.4, 1024, 0.1024 ]);
     expect(closeEnough(grad, expectedGrad)).toBe(true);
   });
-})
+});
+
